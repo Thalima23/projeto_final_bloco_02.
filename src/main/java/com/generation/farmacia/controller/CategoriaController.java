b618801838b4440b630fe3438c3c2ae5f3685dde
Thalitalima23 <thalitalima23@gmail.com>
2025-10-27 11:06:07 -0300

    :sparkles: Construindo CRUD Categoria:pill:

---
+++ src/main/java/com/generation/farmacia/controller/CategoriaController.java
@@ -0,0 +1,73 @@
+package com.generation.farmacia.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.generation.farmacia.model.Categoria;
+import com.generation.farmacia.repository.CategoriaRepository;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/categorias")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class CategoriaController {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    // 1 - Listar todas as categorias//
+    @GetMapping
+    public ResponseEntity<List<Categoria>> getAll() {
+        return ResponseEntity.ok(categoriaRepository.findAll());
+    }
+
+    // 2 - Buscar por ID//
+    @GetMapping("/{id}")
+    public ResponseEntity<Categoria> getById(@PathVariable Long id) {
+        return categoriaRepository.findById(id)
+        		.map(resposta -> ResponseEntity.ok(resposta))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    // 3 - Buscar por nome//
+    @GetMapping("/nome/{nome}")
+    public ResponseEntity<List<Categoria>> getByNome(@PathVariable String nome) {
+    	
+        return ResponseEntity.ok(categoriaRepository.findAllByNomeContainingIgnoreCase(nome));
+    }
+
+    // 4 - Criar nova categoria//
+    @PostMapping
+    public ResponseEntity<Categoria> postCategoria(@Valid @RequestBody Categoria categoria) {
+    	categoria.setId(null);
+    	
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoriaRepository.save(categoria));
+    }
+
+    // 5 - Atualizar categoria//
+    @PutMapping
+    public ResponseEntity<Categoria> put(@Valid @RequestBody Categoria categoria) {
+    	
+        return categoriaRepository.findById(categoria.getId())
+        		.map(resposta -> ResponseEntity.ok().body(categoriaRepository.save(categoria)))
+				.orElse(ResponseEntity.notFound().build());
+    }
+
+    // 6 - Deletar categoria//
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteCategoria(@PathVariable Long id) {
+    	return categoriaRepository.findById(id)
+    			.map(resposta -> {
+    				categoriaRepository.deleteById(id);
+    				return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    			})
+    			.orElse(ResponseEntity.notFound().build());
+    			
+    			}
+}
+
---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,55 @@
+package com.generation.farmacia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotBlank(message = "O nome da categoria é obrigatório!")
+	 @Size(min = 2, max = 100, message= "O atributo nome deve conter no mínimo 2 e no máximo 100 caracteres")
+	 private String nome;
+	
+	 @NotBlank(message = "A descrição é obrigatória!")
+	 @Size(min = 5, max = 100, message= "O produto deve conter uma descrição de no mínimo 5 caracteres e no máximo 100 caracteres ")
+	 private String descricao;
+	 
+	 //Getters and Setters
+
+	 public long getId() {
+		 return id;
+	 }
+
+	 public void setId(Long id) {
+		 this.id = id;
+	 }
+
+	 public String getNome() {
+		 return nome;
+	 }
+
+	 public void setNome(String nome) {
+		 this.nome = nome;
+	 }
+
+	 public String getDescricao() {
+		 return descricao;
+	 }
+
+	 public void setDescricao(String descricao) {
+		 this.descricao = descricao;
+	 }
+	
+	 
+	 
+}
